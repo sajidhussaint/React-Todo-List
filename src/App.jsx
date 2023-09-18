f3a2d766e3ab5656091c7c272d4c5fb5a0177f16
sajid <sajidtmpm@gmail.com>
2023-09-18 09:44:11 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import CustomForm from "./components/CustomForm";
+import TaskList from "./components/TaskList";
+import EditForm from "./components/EditForm";
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+  const [editedTask, setEditedTask] = useState(null);
+  const [isEditing, setIsEditing] = useState(false);
+
+  const addTask = (task) => {
+    setTasks((prevState) => [...prevState, task]);
+  };
+  const deleteTask = (id) => {
+    setTasks((prevState) => prevState.filter((t) => t.id !== id));
+  };
+  const toggleTask = (id) => {
+    setTasks((prevState) =>
+      prevState.map((t) => (t.id === id ? { ...t, checked: !t.checked } : t))
+    );
+  };
+  const updateTask = (task) => {
+    setTasks((prevState) =>
+      prevState.map((t) => (t.id === task.id ? { ...t, name: task.name } : t))
+    );
+    closeEditMode();
+  };
+
+  const closeEditMode = () => {
+    setIsEditing(false);
+  };
+
+  const enterEditMode = (task) => {
+    setEditedTask(task);
+    setIsEditing(true);
+  };
+
+  return (
+    <div className="container">
+      <header>
+        <h1>My Tasks</h1>
+      </header>
+      {isEditing && (
+        <EditForm editedTask={editedTask} updateTask={updateTask} />
+      )}
+
+      <CustomForm addTask={addTask} />
+      {tasks && (
+        <TaskList
+          tasks={tasks}
+          deleteTask={deleteTask}
+          toggleTask={toggleTask}
+          enterEditMode={enterEditMode}
+        />
+      )}
+    </div>
+  );
+}
+
+export default App;
